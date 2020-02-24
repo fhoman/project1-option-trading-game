65b55fa591e1384428f094819c62ac56005f7e2b
Friso <fhoman@gmail.com>
2020-02-24 07:43:31 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,210 +1,224 @@
 // Event lister on start game button
 
 document.getElementById('start-game').addEventListener('click', function (e) {
-    const name = document.getElementById('player-name').value;
-    if (name) {
-        const quiz = new Quiz(name, 2);
-        quiz.startGame(name);
-    } else {
-        alert('fill in your name')
-    }
+  const name = document.getElementById('player-name').value
+  if (name) {
+    const quiz = new Quiz(name, 2)
+    quiz.startGame(name)
+  } else {
+    alert('fill in your name')
+  }
 }, {
-    once: true
+  once: true
 })
 
 // Create class quiz with logic to run the quiz
 
 class Quiz {
+  constructor (name, number) {
+    // Generate new player
+    this.player = new Player(name)
+    this.name = this.player.name
+    this.score = this.player.score
+    this.answers = this.player.answers
+    this.answer = this.player.answer
+    // Render screen class
+    this.screens = new Screens(this.player)
+    // Generate randomArray of questions based on number
+    this.highScores = [{
+      name: 'Erik',
+      score: 100
+    }, {
+      name: 'Pieter',
+      score: 40
+    }, {
+      name: 'Marie',
+      score: 30
+    }]
+    this.number = number
+    this.questionsArray = this.getRandomQuestions()
+    // Set variables for the timer elements
+    this.timeQuestion = 10
+    this.timeTimeOutQuestion = 10000
+    this.timeLeftQuestion = null
+    this.seconds = 0
+    this.rightAnswer = 100
+    this.timeoutID = undefined
+    this.timerID = undefined
+  }
 
-    constructor(name, number) {
-        // Generate new player
-        this.player = new Player(name);
-        this.name = this.player.name;
-        this.score = this.player.score;
-        this.answers = this.player.answers;
-        this.answer = this.player.answer;
-        // Render screen class
-        this.screens = new Screens(this.player);
-        // Generate randomArray of questions based on number
-        this.highScores = [{name:'Erik',score: 100},{name:'Pieter',score: 40},{name:'Marie',score: 30}]
-        this.number = number;
-        this.questionsArray = this.getRandomQuestions();
-        // Set variables for the timer elements
-        this.timeQuestion = 10;
-        this.timeTimeOutQuestion = 10000
-        this.timeLeftQuestion = null
-        this.seconds = 0;
-        this.rightAnswer = 100;
-        this.timeoutID = undefined
-        this.timerID = undefined
+  // Initialise a new game
+  startGame () {
+    this.screens.renderIntroductionScreen()
+    // Add event listeners for different screens
+    this.addEventListenerIntro()
+    this.addEventListenerMultipleQuestions()
+    this.addEventListenerFeedback()
+  }
+
+  getRandomQuestions () {
+    const number = this.number
+    const data = []
+    JSON.parse(JSONdata).forEach(function (element, index) {
+      if (element.category === 'multiple' || 'trade-demo') {
+        data.push(element)
+      }
+      if (index > number - 1) {
+        return data
+      }
+    })
+    return data
+  }
+
+  questionsLoop () {
+    // Check if the array with questions is empty to trigger result screen
+    if (this.questionsArray.length < 1) {
+      this.addScorePlayer({
+        name: this.name,
+        score: this.score
+      })
+      this.screens.renderResultScreen(this.score, this.highScores)
+      this.addEventListenerStart()
+      return
     }
+    // render new question screen based on category
+    this.screens.renderQuestionScreens(this.questionsArray[0], this.score,
+      this.questionsArray[0].category)
+    // Set timer to count down seconds for every question
+    this.setTimer()
+    // Set timeout for question
+    this.timeoutID = setTimeout(() => {
+      // Update score if the timeout is finished
+      this.calculateScore()
+      // no answer was give is updated in the answer array
+      this.player.answers.push({
+        answer: null,
+        state: false,
+        score: 0
+      })
+      // trigger feedback screen and event listener if the timeout is finished
+      this.feedBackLoop()
+    }, this.timeTimeOutQuestion)
+  }
 
-    // Initialise a new game
-    startGame() {
-        
-        this.screens.renderIntroductionScreen();
-        // Add event listeners for different screens        
-        this.addEventListenerIntro()
-        this.addEventListenerMultipleQuestions()
-        this.addEventListenerFeedback()
-    }
+  // Start the feedback loop if the timeout has finished
+  feedBackLoop () {
+    // Render the feedback loop screen if the timeout
+    this.screens.renderFeedbackScreen(this.questionsArray[0], this.score,
+      this.questionsArray[0].category, this.player.answers)
+    // Remove the question from the array after the feedback loop is finished
+    this.questionsArray.splice(0, 1)
+    this.player.answers.splice(0, 1)
+  }
 
-    getRandomQuestions() {
-        const number = this.number
-        const data = [];
-        JSON.parse(JSONdata).forEach(function (element, index) {
-            if (element.category === 'multiple' || 'trade-demo') {
-                data.push(element);
-            }
-            if (index > number - 1) {
-                return data;
-            }
+  calculateScore () {
+    // Check the answer with loop over the array
+    this.questionsArray[0].answers.forEach(this.checkAnswersMultiple.bind(this))
+  }
+
+  checkAnswersMultiple (element) {
+    if (this.answer === element.answer) {
+      if (element.state === true) {
+        this.score += this.rightAnswer
+        this.score += this.timeLeftQuestion
+        this.player.answers.push({
+          answer: element.answer,
+          state: element.state,
+          score: this.rightAnswer + this.timeLeftQuestion
         })
-        return data;
-    }
-
-    questionsLoop() {
-        // Check if the array with questions is empty to trigger result screen
-        if (this.questionsArray.length < 1) {
-            this.addScorePlayer({
-                'name': this.name,
-                'score': this.score
-            })
-            this.screens.renderResultScreen(this.score, this.highScores);
-            this.addEventListenerStart()
-            return
-        }
-        // render new question screen based on category
-        this.screens.renderQuestionScreens(this.questionsArray[0], this.score,
-            this.questionsArray[0].category)
-        // Set timer to count down seconds for every question
-        this.setTimer()
-        // Set timeout for question
-        this.timeoutID = setTimeout(() => {
-            // Update score if the timeout is finished
-            this.calculateScore();
-            // no answer was give is updated in the answer array
-            this.player.answers.push({
-                'answer': null,
-                'state': false,
-                'score': 0
-            })
-            // trigger feedback screen and event listener if the timeout is finished          
-            this.feedBackLoop()
-        }, this.timeTimeOutQuestion)
-    }
-    // Start the feedback loop if the timeout has finished
-    feedBackLoop() {
-        // Render the feedback loop screen if the timeout
-        this.screens.renderFeedbackScreen(this.questionsArray[0], this.score,
-            this.questionsArray[0].category, this.player.answers);
-        // Remove the question from the array after the feedback loop is finished
-        this.questionsArray.splice(0, 1);
-        this.player.answers.splice(0, 1);
-    }
-    calculateScore() {
-        // Check the answer with loop over the array
-        this.questionsArray[0].answers.forEach(this.checkAnswersMultiple.bind(this))
-    }
-    checkAnswersMultiple(element) {
-        if (this.answer === element.answer) {
-            if (element.state === true) {
-                this.score += this.rightAnswer;
-                this.score += this.timeLeftQuestion;
-                this.player.answers.push({
-                    'answer': element.answer,
-                    'state': element.state,
-                    'score': this.rightAnswer + this.timeLeftQuestion
-                })
-            } else {
-                this.player.answers.push({
-                    'answer': element.answer,
-                    'state': element.state,
-                    'score': 0
-                })
-            }
-        }
-    }
-
-    setTimer() {
-        this.timeElement = document.getElementById('timer');
-        this.timerID = setInterval(countdown.bind(this), 1000);
-
-        function countdown() {
-            if (this.timeQuestion === 0) {
-                clearTimeout(this.timerID);
-            } else {
-                this.timeElement.innerHTML = `${this.timeQuestion}`;
-                this.timeQuestion--;
-            }
-        }
-    }
-    calculateTimeBonus() {
-        if (time > 0) {
-            this.score += this.rightAnswer;
-            this.score += time;
-            this.player.answers.push({
-                'score': this.rightAnswer + time
-            })
-        } else if (time < 1) {
-            this.player.answers.push({
-                'score': 0,
-            })
-        }
-    }
-    addEventListenerStart() {
-        this.screens.divElement.addEventListener('click', function (e) {
-            if (e.target.id === 'start-game') {
-                location.reload()
-            }
-        }, {
-            once: true
+      } else {
+        this.player.answers.push({
+          answer: element.answer,
+          state: element.state,
+          score: 0
         })
+      }
     }
-    // Event listeners for all rendered screens
-    addEventListenerIntro() {
-        this.screens.divElement.addEventListener('click', (e) => {
-            if (e.target.id === 'start-questions') {
-                this.questionsLoop();
-            }
-        })
-    }
-    addEventListenerMultipleQuestions() {
-        this.screens.divElement.addEventListener('click', (e) => {
-            if (e.target.className === 'answer') {
-                clearTimeout(this.timeoutID)
-                clearTimeout(this.timerID)
-                this.answer = e.target.innerText;
-                this.timeLeftQuestion = this.timeQuestion
-                this.calculateScore()
-                this.feedBackLoop();
-            }
-        })
-    }
-    addEventListenerFeedback() {
-        this.screens.divElement.addEventListener('click', (e) => {
-            if (e.target.id === 'next-question') {
-                this.questionsLoop()
-            }
-        })
-    }
+  }
 
-    addScorePlayer(player) {
-        console.log(this.highScores)
-        this.highScores.push(player);
-        this.highScores.sort(function (a, b) {
-            return b.score - a.score;
-        });    
-        console.log(this.highScores)
+  setTimer () {
+    this.timeElement = document.getElementById('timer')
+    this.timerID = setInterval(countdown.bind(this), 1000)
+
+    function countdown () {
+      if (this.timeQuestion === 0) {
+        clearTimeout(this.timerID)
+      } else {
+        this.timeElement.innerHTML = `${this.timeQuestion}`
+        this.timeQuestion--
+      }
     }
+  }
+
+  calculateTimeBonus () {
+    if (time > 0) {
+      this.score += this.rightAnswer
+      this.score += time
+      this.player.answers.push({
+        score: this.rightAnswer + time
+      })
+    } else if (time < 1) {
+      this.player.answers.push({
+        score: 0
+      })
+    }
+  }
+
+  addEventListenerStart () {
+    this.screens.divElement.addEventListener('click', function (e) {
+      if (e.target.id === 'start-game') {
+        location.reload()
+      }
+    }, {
+      once: true
+    })
+  }
+
+  // Event listeners for all rendered screens
+  addEventListenerIntro () {
+    this.screens.divElement.addEventListener('click', (e) => {
+      if (e.target.id === 'start-questions') {
+        this.questionsLoop()
+      }
+    })
+  }
+
+  addEventListenerMultipleQuestions () {
+    this.screens.divElement.addEventListener('click', (e) => {
+      if (e.target.className === 'answer') {
+        clearTimeout(this.timeoutID)
+        clearTimeout(this.timerID)
+        this.answer = e.target.innerText
+        this.timeLeftQuestion = this.timeQuestion
+        this.calculateScore()
+        this.feedBackLoop()
+      }
+    })
+  }
+
+  addEventListenerFeedback () {
+    this.screens.divElement.addEventListener('click', (e) => {
+      if (e.target.id === 'next-question') {
+        this.questionsLoop()
+      }
+    })
+  }
+
+  addScorePlayer (player) {
+    console.log(this.highScores)
+    this.highScores.push(player)
+    this.highScores.sort(function (a, b) {
+      return b.score - a.score
+    })
+    console.log(this.highScores)
+  }
 }
 
-
 class Player {
-    constructor(name) {
-        this.name = name;
-        this.score = 0;
-        this.answers = [];
-        this.answer = null;
-    }
+  constructor (name) {
+    this.name = name
+    this.score = 0
+    this.answers = []
+    this.answer = null
+  }
 }
--- js/questions.js
+++ js/questions.js
@@ -1,99 +1,96 @@
 const JSONdata = JSON.stringify([{
-    //    id: 1,
-    //    question: `Buy a long straddle Tesla with a strike of 800`,
-    //    answer: [{
-     //       'type': 'Call',
-    //        'action': 'Buy',
-    //        'strike': 800
-    //    }, {
-    //        'type': 'Put',
-    //        'action': 'Buy',
-    //        'strike': 800
-     //   }],
-    //    category: 'trade-demo',
-     //   serie: {
-    //        ISIN: 'Tesla',
-    //        stock: 'TSLA FEB 2020',
-    //        date: 'FEB 2020'
-    //    },
-   //     quotes: [
-    //        ['1,10', '1,14', '895', '1,16', '1,16'],
-    //        ['1,10', '1,14', '925', '1,16', '1,16'],
-    //        ['1,10', '1,14', '900', '1,16', '1,16'],
-    //        ['1,10', '1,14', '930', '1,16', '1,16'],
-    //        ['1,10', '1,14', '905', '1,16', '1,16'],
-     //       ['1,10', '1,14', '935', '1,16', '1,16'],
-     //       ['1,10', '1,14', '910', '1,16', '1,16'],
-      //      ['1,10', '1,14', '940', '1,16', '1,16'],
-     //       ['1,10', '1,14', '915', '1,16', '1,16'],
-     //       ['1,10', '1,14', '945', '1,16', '1,16'],
-     //       ['1,10', '1,14', '920', '1,16', '1,16']
-    //    ]
-    //},
+  //    id: 1,
+  //    question: `Buy a long straddle Tesla with a strike of 800`,
+  //    answer: [{
+  //       'type': 'Call',
+  //        'action': 'Buy',
+  //        'strike': 800
+  //    }, {
+  //        'type': 'Put',
+  //        'action': 'Buy',
+  //        'strike': 800
+  //   }],
+  //    category: 'trade-demo',
+  //   serie: {
+  //        ISIN: 'Tesla',
+  //        stock: 'TSLA FEB 2020',
+  //        date: 'FEB 2020'
+  //    },
+  //     quotes: [
+  //        ['1,10', '1,14', '895', '1,16', '1,16'],
+  //        ['1,10', '1,14', '925', '1,16', '1,16'],
+  //        ['1,10', '1,14', '900', '1,16', '1,16'],
+  //        ['1,10', '1,14', '930', '1,16', '1,16'],
+  //        ['1,10', '1,14', '905', '1,16', '1,16'],
+  //       ['1,10', '1,14', '935', '1,16', '1,16'],
+  //       ['1,10', '1,14', '910', '1,16', '1,16'],
+  //      ['1,10', '1,14', '940', '1,16', '1,16'],
+  //       ['1,10', '1,14', '915', '1,16', '1,16'],
+  //       ['1,10', '1,14', '945', '1,16', '1,16'],
+  //       ['1,10', '1,14', '920', '1,16', '1,16']
+  //    ]
+  // },
 
-   // {
-        id: 2,
-        question: 'Which option strategy is this profit loss from?',
-        category: 'multiple',
-        answers: [{
-            answer: 'Iron condor',
-            state: false
-        }, {
-            answer: 'Long call spread',
-            state: false
-        }, {
-            answer: 'Long straddle',
-            state: false
-        }, {
-            answer: 'Long put spread',
-            state: true
-        }],
-        image: 'optiestrategie_longputspread_v1.jpg',
-        feedback: 'This is the profit loss diagram of the option strategy Long put spread'
-    },
-    {
-        id: 3,
-        question: 'Which option strategy is this profit loss from?',
-        category: 'multiple',
-        answers: [{
-            answer: 'Short strangle',
-            state: false
-        }, {
-            answer: 'Long call spread',
-            state: false
-        }, {
-            answer: 'Long straddle',
-            state: true
-        }, {
-            answer: 'Long call',
-            state: false
-        }],
-        image: 'optiestrategie_longstraddle_v1.jpg',
-        feedback: 'This is the profit/loss diagram of the option strategy Long straddle'
-    },
+  // {
+  id: 2,
+  question: 'Which option strategy is this profit loss from?',
+  category: 'multiple',
+  answers: [{
+    answer: 'Iron condor',
+    state: false
+  }, {
+    answer: 'Long call spread',
+    state: false
+  }, {
+    answer: 'Long straddle',
+    state: false
+  }, {
+    answer: 'Long put spread',
+    state: true
+  }],
+  image: 'optiestrategie_longputspread_v1.jpg',
+  feedback: 'This is the profit loss diagram of the option strategy Long put spread'
+},
+{
+  id: 3,
+  question: 'Which option strategy is this profit loss from?',
+  category: 'multiple',
+  answers: [{
+    answer: 'Short strangle',
+    state: false
+  }, {
+    answer: 'Long call spread',
+    state: false
+  }, {
+    answer: 'Long straddle',
+    state: true
+  }, {
+    answer: 'Long call',
+    state: false
+  }],
+  image: 'optiestrategie_longstraddle_v1.jpg',
+  feedback: 'This is the profit/loss diagram of the option strategy Long straddle'
+},
 
-    {
-        id: 4,
-        question: 'Which option strategy is this profit loss from?',
-        category: 'multiple',
-        answers: [{
-            answer: 'Butterfly',
-            state: false
-        }, {
-            answer: 'Long call spread',
-            state: false
-        }, {
-            answer: 'Long time spread',
-            state: true
-        }, {
-            answer: 'Short put spread',
-            state: false
-        }],
-        image: 'optiestrategie_longtimespread_v1.jpg',
-        feedback: 'This is the profit/loss diagram of the option strategy Long time spread'
-    }
+{
+  id: 4,
+  question: 'Which option strategy is this profit loss from?',
+  category: 'multiple',
+  answers: [{
+    answer: 'Butterfly',
+    state: false
+  }, {
+    answer: 'Long call spread',
+    state: false
+  }, {
+    answer: 'Long time spread',
+    state: true
+  }, {
+    answer: 'Short put spread',
+    state: false
+  }],
+  image: 'optiestrategie_longtimespread_v1.jpg',
+  feedback: 'This is the profit/loss diagram of the option strategy Long time spread'
+}
 ])
 
-// Order 10 putoptions Tesla on the CME 
-// Buy a long call spread on Facebook with strikes 200 and 220 and 
-// Buy a short strangle DAX with strikes 3000 and 3030
